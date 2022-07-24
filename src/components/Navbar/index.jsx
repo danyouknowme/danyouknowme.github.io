d5d9ad75cce8d855bb5a82ad82946d4d876711c3
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-07-24 20:54:10 +0700

    Fixed navbar background

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,27 +3,34 @@ import "./navbar.scss";
 
 const Navbar = ({ renderContent }) => {
 	return (
-		<div
-			className="navbar__container nes-container is-rounded is-primary"
-			style={{ top: `${renderContent ? "0" : "-104px"}` }}
-		>
-			<h1>Thanathip S.</h1>
-			<ul>
-				<li>Experience</li>
-				<li>Project</li>
-				<li>
-					<a
-						href="img/Thanathip_Suwannakhot.pdf"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<button type="button" className="nes-btn is-error">
-							RESUME
-						</button>
-					</a>
-				</li>
-			</ul>
-		</div>
+		<>
+			<div
+				className="navbar__container nes-container is-rounded is-primary"
+				style={{ top: `${renderContent ? "0" : "-104px"}` }}
+			>
+				{/* <h1>Thanathip S.</h1> */}
+				<div></div>
+				<ul>
+					<li>Experience</li>
+					<li>Project</li>
+					<li>
+						<a
+							href="img/Thanathip_Suwannakhot.pdf"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<button type="button" className="nes-btn is-error">
+								RESUME
+							</button>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div
+				className="navbar-bg__container"
+				style={{ top: `${renderContent ? "0" : "-104px"}` }}
+			></div>
+		</>
 	);
 };
 
