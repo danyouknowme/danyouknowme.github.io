845ba5589ae58ea1b1b9710a4eddcec39a5bbcbf
= <thanathip.suw@gmail.com>
2022-07-25 16:58:03 +0700

    Import nes icons style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import './nes-icon.scss';
 import "nes.css/css/nes.min.css";
+import "nes.icons/css/nes-icons.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
