db00d31fd2e0eb3d4d61e63fe8913ca2bf65b2c0
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-07-27 14:36:33 +0700

    Change profile image

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import "./home.scss";
 
 const Home = () => {
-  return (
-    <div className="home__section">
-      <div className="home-top__container">
-        <div className="profile-image__container">
-          <img className="profile" src="img/profile2.jpg" alt="" />
-          <img className="frame" src="img/profile-frame.png" alt="" />
-        </div>
-        <div className="nes-balloon from-left">
-          <h2>Thanathip Suwannakhot</h2>
-          <p>Software Engineering Student</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="home__section">
+			<div className="home-top__container">
+				<div className="profile-image__container">
+					<img className="profile" src="img/dan.jpg" alt="" />
+					<img className="frame" src="img/profile-frame.png" alt="" />
+				</div>
+				<div className="nes-balloon from-left">
+					<h2>Thanathip Suwannakhot</h2>
+					<p>Software Engineering Student</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
