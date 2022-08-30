7c3c4241b71d8c12766e3ce6ab116706520812d8
danyouknowme <thanathip.suw@gmail.com>
2022-08-30 23:01:51 +0700

    add profile image and fixed navbar color

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,24 +2,23 @@ import React from "react";
 import "./home.scss";
 
 const Home = () => {
-	return (
-		<div className="home__section">
-			<div className="home-top__container">
-				<div className="profile-image__container">
-					<img className="profile" src="img/dan.jpg" alt="" />
-					<img className="frame" src="img/profile-frame.png" alt="" />
-				</div>
-				<div className="nes-balloon from-left">
-					<h2>Thanathip Suwannakhot</h2>
-					<p>Software Engineering Student</p>
-				</div>
-				<div className="nes-container is-rounded">
-					<h2>Thanathip Suwannakhot</h2>
-					<p>Software Engineering Student</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="home__section">
+      <div className="home-top__container">
+        <div className="profile-image__container">
+          <img className="profile" src="img/rpfggina.png" alt="" />
+        </div>
+        <div className="nes-balloon from-left">
+          <h2>Thanathip Suwannakhot</h2>
+          <p>Software Engineering Student</p>
+        </div>
+        <div className="nes-container is-rounded">
+          <h2>Thanathip Suwannakhot</h2>
+          <p>Software Engineering Student</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
