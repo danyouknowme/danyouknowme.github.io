3ac476595803e3d479d5731ec496f2c63dda3e6f
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2022-12-08 04:41:42 +0700

    fix: Added navbar and content on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react'
+import { Navbar, Scene, Quote } from './components'
+import {
+  Article,
+  ContentSection,
+  GlobalStyle,
+  MainSection,
+} from './globalStyles'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <GlobalStyle />
+      <Navbar />
+      <MainSection>
+        <Scene />
+        <Article>
+          <ContentSection>
+            <Quote />
+          </ContentSection>
+        </Article>
+      </MainSection>
+    </Fragment>
+  )
 }
-
-export default App;
