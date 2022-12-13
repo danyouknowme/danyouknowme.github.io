196968806165bdd6cd7eb99d9455297a1ca25347
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2022-12-14 00:48:24 +0700

    feat: Added content and styling experience page

--- src/screens/Experience/index.tsx
+++ src/screens/Experience/index.tsx
@@ -1,11 +1,83 @@
+import { Fragment } from 'react'
+import { GiBriefcase } from 'react-icons/gi'
 import { Article, ContentWrapper } from '../../styles/global'
-import { ContentSection } from './style'
+import { toCapitalized } from '../../utils/string'
+import {
+  CompanyComponent,
+  ContentSection,
+  HorizontalLine,
+  ImageContainer,
+  TextContainer,
+  WorkingContainer,
+  WorkingItem,
+  WorkingList,
+} from './style'
+
+const data = [
+  {
+    company: 'awayfromus',
+    position: 'Frontend Developer Part-Time',
+    imageSource: 'https://awayfromus.dev/afu-logo.png',
+    jobs: [
+      'Create game scripts for FiveM which is an online mod of GTA V',
+      'Contributor to the web application that sells the available game scripts',
+      'React + NodeJS + Typescript',
+      'HTML + CSS + Javascript + VueJS',
+      'December, 2021',
+    ],
+  },
+  {
+    company: 'agoda',
+    position: 'QA Engineer Intern, Frontend Team',
+    imageSource:
+      'https://seeklogo.com/images/A/agoda-logo-8C565D040A-seeklogo.com.png',
+    jobs: [
+      'Refactor one of the test framework to execute end-to-end testing in steps',
+      'C# + .NET Core',
+      'SQL',
+      'June, 2022',
+    ],
+  },
+]
 
 export default function Experience() {
   return (
     <Article>
       <ContentWrapper>
-        <ContentSection>In progress...</ContentSection>
+        <ContentSection>
+          <WorkingContainer>
+            <div className='working__title'>
+              <GiBriefcase fontSize={32} />
+              <h2>Working Experiences</h2>
+            </div>
+            <HorizontalLine />
+            <WorkingList>
+              {data.map((work, index) => (
+                <Fragment key={index}>
+                  {index !== 0 && <HorizontalLine />}
+                  <WorkingItem>
+                    <ImageContainer>
+                      <img src={work.imageSource} alt={work.company} />
+                    </ImageContainer>
+                    <TextContainer>
+                      <div className='working__text'>
+                        <h3>{work.position}</h3>
+                        <CompanyComponent>
+                          @{toCapitalized(work.company)}
+                        </CompanyComponent>
+                      </div>
+                      <ul>
+                        {work.jobs.map((job, index) => (
+                          <li key={index}>{job}</li>
+                        ))}
+                      </ul>
+                    </TextContainer>
+                  </WorkingItem>
+                </Fragment>
+              ))}
+            </WorkingList>
+          </WorkingContainer>
+        </ContentSection>
       </ContentWrapper>
     </Article>
   )
--- src/screens/Experience/style.ts
+++ src/screens/Experience/style.ts
@@ -1,7 +1,93 @@
+import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
 export const ContentSection = styled.div`
   width: 100%;
   margin: 0 auto;
   padding: 0 24px;
+`
+
+export const WorkingContainer = styled(motion.div)`
+  margin: 1.4rem 0;
+
+  .working__title {
+    display: flex;
+    align-items: center;
+    
+    h2 {
+      font-size: 1.6rem;
+      font-weight: 600;
+      margin-left: 0.4rem;
+    }
+  }
+`
+
+export const WorkingList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 1rem;
+`
+
+export const WorkingItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0.8rem 0;
+  gap: 2rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+export const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  min-width: calc(100% / 3);
+
+  img {
+    max-width: 200px;
+    max-height: 120px;
+    margin: 20px;
+  }
+`
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .working__text {
+    display: flex;
+    align-items: center;
+    margin-bottom: 0.5rem;
+    
+    h3 {
+      font-size: 1.3rem;
+      font-weight: 500;
+    }
+  }
+    
+  ul {
+    margin: 0 2.5rem;
+
+    li {
+      font-size: 1.1rem;
+    }
+  }
+`
+
+export const HorizontalLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.text};
+  opacity: 0.2;
+  margin: 1rem 0;
+`
+
+export const CompanyComponent = styled.div`
+  margin-left: 0.5rem;
+  background: ${({ theme }) => theme.text};
+  color: ${({ theme }) => theme.background};
+  padding: 0.2rem 0.5rem;
+  border-radius: 0.3rem;
 `
